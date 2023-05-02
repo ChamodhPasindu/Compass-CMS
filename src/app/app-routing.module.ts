616e14830206db728f76c756823deba47d115816
ChamodhPasindu <chamodhpasindu2000@gmail.com>
2023-05-02 09:08:29 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/core/login/login.component';
+import { AuthGuardService } from './service/auth-guard/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'home',
+    canActivate: [AuthGuardService],
+    loadChildren: () =>
+      import('./components/core/layout/layout.module').then(
+        (m) => m.LayoutModule
+      ),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabledBlocking',
+    useHash: false
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
